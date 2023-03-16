3c74414e9bb6baed72c163d46f704d3a45dafcfb
JonathanEKP <jonathanvaldez2001@gmail.com>
2023-03-15 18:48:07 -0600

    Busqueda de ciudades funcionales, faltan agregar más funciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Card from './Components/Cards/Card'
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Card/>
     </div>
   )
 }
--- src/Components/Cards/Card.jsx
+++ src/Components/Cards/Card.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
-
-
+import React, { useContext } from "react";
+import "./Card.css";
+import { WeatherContext } from "../../Context/WeatherContext";
 function Card() {
+  const { data } = useContext(WeatherContext);
+
   return (
-    <div>
-        
+    <div className="d-flex justify-content-center mt-3 mt-lg-4 py-lg-3">
+      <div className="card rounded-4 mt-lg-3" id="card-container">
+        <div className="card-body">
+          <h1 className="card-title text-center">
+            {data === undefined ? 0 : data.current.temp_c}
+            <span className="mt-2 fs-3 position-fixed">&nbsp;°C</span>
+          </h1>
+          <div className="row g-0">
+            <div className="col-12 d-flex justify-content-center mt-1">
+              {data === undefined ? (
+                <div className="spinner-border text-light" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </div>
+              ) : (
+                <img
+                  className="condition-icon"
+                  src={data.current.condition.icon}
+                  alt="Weather condition icon"
+                />
+              )}
+            </div>
+            <div className="col-12 mt-1">
+              <h3 className="text-center">
+                {data === undefined ? "" : data.current.condition.text}
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
